6b65989bcfe33bc18dda9e2e9f86ee04d3df7779
Jan Kundrát <jkt@flaska.net>
2013-11-27 18:24:20 +0100

    GUI: show message subject in the composer's window title

--- src/Gui/ComposeWidget.cpp
+++ src/Gui/ComposeWidget.cpp
@@ -122,6 +122,7 @@ ComposeWidget::ComposeWidget(MainWindow *mainWindow, QSettings *settings, MSA::M
     connect(ui->mailText, SIGNAL(urlsAdded(QList<QUrl>)), SLOT(slotAttachFiles(QList<QUrl>)));
     connect(ui->mailText, SIGNAL(sendRequest()), SLOT(send()));
     connect(ui->mailText, SIGNAL(textChanged()), SLOT(setMessageUpdated()));
+    connect(ui->subject, SIGNAL(textChanged(QString)), SLOT(updateWindowTitle()));
 
     FromAddressProxyModel *proxy = new FromAddressProxyModel(this);
     proxy->setSourceModel(m_mainWindow->senderIdentitiesModel());
@@ -1038,6 +1039,15 @@ void ComposeWidget::autoSaveDraft()
 void ComposeWidget::setMessageUpdated()
 {
     m_messageEverEdited = m_messageUpdated = true;
+}
+
+void ComposeWidget::updateWindowTitle()
+{
+    if (ui->subject->text().isEmpty()) {
+        setWindowTitle(tr("Compose Mail"));
+    } else {
+        setWindowTitle(tr("%1 - Compose Mail").arg(ui->subject->text()));
+    }
 }
 
 }
